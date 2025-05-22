c2c7cc59b1c51220d6f0984b7cfdc0a943bace8f
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-22 10:40:02 +0500

    created the template and renders the templete and tell the django in setting.py that here is the new app

--- mytennis_club/mytennis_club/settings.py
+++ mytennis_club/mytennis_club/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members', #  We have to tell Django that a new app is created.
 ]
 
 MIDDLEWARE = [
