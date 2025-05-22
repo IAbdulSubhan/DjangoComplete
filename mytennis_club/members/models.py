cf0cb78af86abe264984a0611619d8b0a6f5b5f4
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-22 10:47:00 +0500

    working with model and make migration and migrate it

--- mytennis_club/members/models.py
+++ mytennis_club/members/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# add a Member table by creating a Member class, and describe the table fields in it
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
