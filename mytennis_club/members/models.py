a6eef4b3e4670ba4a3fc0cd1ffcc9b327c8f79ff
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-22 10:58:17 +0500

    method to print the data we can call it in shell to get result

--- mytennis_club/members/models.py
+++ mytennis_club/members/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return f"FirstName: {self.firstname} LastName: {self.lastname}"
+    
