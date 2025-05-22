39f051552206fbf837d2cc23ab502197881bd862
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-22 10:29:49 +0500

    hello world in django with routes

--- mytennis_club/members/urls.py
+++ mytennis_club/members/urls.py
@@ -3,7 +3,7 @@ from . import views #access to all view
 
 
 urlpatterns = [
-    path('members/', views.members, name="members")
+    path('members/', views.members, name="members") #name is like we do as in rails like memebers_path etc
 ]
 
 
--- mytennis_club/mytennis_club/urls.py
+++ mytennis_club/mytennis_club/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('', include(members.urls)),
+    path('', include('members.urls')),   # yeah member k routes py ly jata hai agay uski marzi jo karay
     path('admin/', admin.site.urls),
 ]
