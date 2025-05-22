8ffcfb750286746e05d3ce4d7704e614173799a9
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-22 12:04:54 +0500

    changing of modal it makes a migration we maded and migrate that

--- mytennis_club/members/models.py
+++ mytennis_club/members/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from datetime import date
 
 # add a Member table by creating a Member class, and describe the table fields in it
 
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(default = 0)
+    joined_date = models.DateField(default = date.today)
 
 
     def __str__(self):
