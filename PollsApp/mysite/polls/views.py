0386c365815bc28d8323be4043c636ded617e717
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-25 14:06:25 +0500

    question show page setting up

--- PollsApp/mysite/polls/views.py
+++ PollsApp/mysite/polls/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+from django.http import Http404
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list" : latest_question_list }
-    return HttpResponse(template.render(context, request))
-
+    return render(request, 'polls/index.html', context)
+#show page / detail page like polls/2/
 def detail(request, question_id):
-    return HttpResponse("You're looking at the results of  question%s." %question_id)
+    try: 
+        question = Question.objects.get(pk = question_id)
+        context = {"question" : question}
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', context)
+
 
 def results(request, question_id):
     response = "You are looking at the result of question %s."
