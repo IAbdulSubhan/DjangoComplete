3cb588bd61a9d5c5a8208464977fb3acf5e07b9f
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-27 00:54:58 +0500

    importing or registering the choices in question class and rendered the choices with one quesiton

--- PollsApp/mysite/polls/admin.py
+++ PollsApp/mysite/polls/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     # for dozen of fild we will split our for to make user friendly
     #None means: no section title.``
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
