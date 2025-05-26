41a4548827704de39f8a84f37535cd218f955695
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-05-27 00:06:38 +0500

    spliting form fileds under specific filds to make user friendly

--- PollsApp/mysite/polls/admin.py
+++ PollsApp/mysite/polls/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    # for dozen of fild we will split our for to make user friendly
+    #None means: no section title.``
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
