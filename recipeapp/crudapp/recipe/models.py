b0dcbba330609ef44f86a4fcba1b3c322da37d95
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-06-02 20:31:49 +0500

    adding field in model and another file utils.py for other methods kind of helper in rail

--- recipeapp/crudapp/recipe/models.py
+++ recipeapp/crudapp/recipe/models.py
@@ -6,5 +6,10 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='recipes/')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+    
 
 
---
+++ recipeapp/crudapp/recipe/utils.py
@@ -0,0 +1,11 @@
+from django.utils.text import slugify
+import uuid 
+from .models import Recipe
+
+
+def generate_slug(title: str)->str:
+    slug = slugify(title)
+
+    while(Recipe.objects.filter(slug=slug).exists):
+        slug = f'{slugify(title)}-{str(uuid.uuid4())[:4]}'
+    return slug
