821b8e74597ac97b55a2b9f55b640e7e2df4fcb1
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-06-02 20:55:20 +0500

    fixed an error that is --> cannot import name 'Recipe' from partially initialized module 'recipe.models' (most likely due to a circular import)

--- recipeapp/crudapp/recipe/utils.py
+++ recipeapp/crudapp/recipe/utils.py
@@ -1,10 +1,10 @@
 from django.utils.text import slugify
 import uuid 
-from .models import Recipe
+
 
 
 def generate_slug(title: str)->str:
-    
+    from .models import Recipe
     slug = slugify(title)
 
     while(Recipe.objects.filter(slug=slug).exists):
